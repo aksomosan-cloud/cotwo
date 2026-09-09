db7c85f5cca55410fb58a783a6c06876bc3f81d1
aksomosan-cloud <aksomosan@gmail.com>
2026-09-09 17:13:56 +0300

    update code

--- firebase-sync.js
+++
@@ -1,9 +0,0 @@
-import { db, doc, updateDoc, serverTimestamp } from './firebase.js';
-
-export const syncDraft = async draft => {
-  if (!draft.firebaseDocumentId) return;
-  await updateDoc(doc(db, 'users', draft.firebaseDocumentId), {
-    ...draft,
-    updatedAt: serverTimestamp()
-  });
-};
